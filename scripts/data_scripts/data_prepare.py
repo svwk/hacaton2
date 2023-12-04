3a17cf05e9af8020288013d75c67f01331364e7c
DmitriiKrasnov <dmitriy.m.krasnov@gmail.com>
2023-12-04 22:09:58 +0300

    Data preparation

--- scripts/data_scripts/data_prepare.py
+++ scripts/data_scripts/data_prepare.py
@@ -3,6 +3,8 @@
 
 from data_methods import create_stage
 
+import pandas as pd
+import numpy as np
 
 def prepare_dataset(source_dataset):
     """
@@ -11,6 +13,19 @@ def prepare_dataset(source_dataset):
     """
 
     df = source_dataset.copy()
+
+    df['SkillFactory_Id'] = df['SkillFactory_Id'].astype('int')
+    df['BirthDate'] = pd.to_datetime(df['BirthDate'])
+    df['education'] = df['education'].astype('category')
+    df['employment status'] = df['employment status'].astype('category')
+    df['Value'] = df['Value'].astype('category')
+    df['JobStartDate'] = pd.to_datetime(df['JobStartDate'])
+    df['Gender'] = np.where(df['Gender'] > 0, 1, 0)
+    df['Family status'] = df['Family status'].astype('category')
+    df['ChildCount'] = df['ChildCount'].fillna(0).astype('int')
+    df['SNILS'] = df['SNILS'].fillna(0).astype('int')
+    df['Goods_category'] = df['Goods_category'].astype('category')
+    df['Merch_code'] = df['Merch_code'].astype('category')
 
     return df
 
--- scripts/data_scripts/fill_na.py
+++ scripts/data_scripts/fill_na.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from data_methods import create_stage
-from datetime import date
+from datetime import datetime
 
 
 def fill_na_in_dataset(source_dataset):
@@ -21,7 +21,7 @@ def fill_na_in_dataset(source_dataset):
     mode_family_status = df['Family status'].value_counts().idxmax()
 
     df['Value'] = df['Value'].fillna('Нет стажа')
-    df['JobStartDate'] = df['JobStartDate'].fillna(date.today())
+    df['JobStartDate'] = df['JobStartDate'].fillna(datetime.now())
     df['Gender'] = df['Gender'].fillna(mode_gender)
     df['Family status'] = df['Family status'].fillna(mode_family_status)
     df['ChildCount'] = df['ChildCount'].fillna(0)
