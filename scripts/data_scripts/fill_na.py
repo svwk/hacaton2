1067b3323729ce0c5e952284efd3db7233f42b03
Солодухин Артём Валерьевич <artiom.soloduhin@gmail.com>
2023-12-04 20:34:12 +0300

    FillNa edited

--- scripts/data_scripts/fill_na.py
+++ scripts/data_scripts/fill_na.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from data_methods import create_stage
+from datetime import date
 
 
 def fill_na_in_dataset(source_dataset):
@@ -9,8 +10,14 @@ def fill_na_in_dataset(source_dataset):
     Заполнение пропусков
     :param source_dataset:  Исходный датасет
     """
-
     df = source_dataset.copy()
+
+    df['Value'] = df['Value'].fillna('Нет стажа')
+    df['JobStartDate'] = df['JobStartDate'].fillna(date.today())
+    df['gender'] = df['gender'].fillna(df.gender.mode())
+    df['Family status'] = df['Family status'].fillna(df['Family status'].mode())
+    df['ChildCount'] = df['ChildCount'].fillna(0)
+
 
     return df
 
