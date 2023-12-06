2c968137cd76168c40c7c1a2fc331374850adb2c
Солодухин Артём Валерьевич <artiom.soloduhin@gmail.com>
2023-12-06 21:18:21 +0300

    Refactoring

--- scripts/data_scripts/fix_errors.py
+++ scripts/data_scripts/fix_errors.py
@@ -7,8 +7,8 @@ from dateutil.relativedelta import relativedelta
 import pandas as pd
 
 #Прожиточный минимум
-min_adult = 15669
-min_child = 13944
+ADULT_LIVING_WAGE = 15669
+CHILD_LIVING_WAGE = 13944
 
 def fix_errors_in_dataset(source_dataset):
     """
@@ -23,19 +23,24 @@ def fix_errors_in_dataset(source_dataset):
 
     return df
 
-def fix_expense(raw):
-    real_expense = min_adult
-    child_expense = min_child * raw['ChildCount']
+def fix_expense(application):
+    """
+    Исправление аномалий в столбце с тратами
+    :param application:  Строка датасета
+    """
 
-    if(check_family_status(raw['Family status'])):
-        real_expense += min_adult
+    real_expense = ADULT_LIVING_WAGE
+    child_expense = CHILD_LIVING_WAGE * application['ChildCount']
+
+    if(check_family_status(application['Family status'])):
+        real_expense += ADULT_LIVING_WAGE
 
     real_expense += child_expense
 
-    if(real_expense > raw['MonthExpense']):
-        raw['MonthExpense'] = real_expense
+    if(real_expense > application['MonthExpense']):
+        application['MonthExpense'] = real_expense
 
-    return raw
+    return application
 
 def check_family_status(status):
   status = str(status)
@@ -45,40 +50,44 @@ def check_family_status(status):
   else:
     return False
 
-def fix_work_age(raw):
-  work_age = float(values_to_num(raw['Value']))
-  age = time_diff(raw['BirthDate'])
+def fix_work_age(application):
+    """
+    Исправление аномалий в столбце со стажем работы
+    :param application:  Строка датасета
+    """
 
-  work_age_from_now_to_start_date = time_diff(raw['JobStartDate'])
+    work_age = int(values_to_num(application['Value']))
+    age = time_diff(application['BirthDate'])
 
-  if(age < 23 and work_age < age / 2):
-      raw['Value'] = 'Нет стажа'
-      return raw
+    work_age_from_now_to_start_date = time_diff(application['JobStartDate'])
 
-  if(work_age < work_age_from_now_to_start_date):
-    work_age = work_age_from_now_to_start_date
-    raw['Value'] = num_to_cat(work_age)
+    if(age < 20 and work_age < age / 2):
+        application['Value'] = 'Нет стажа'
+        return application
 
-  return raw
+    if(work_age < work_age_from_now_to_start_date):
+        work_age = work_age_from_now_to_start_date
+        application['Value'] = num_to_cat(work_age)
+        return application
 
-def values_to_num(line):
-    line = str(line)
+def values_to_num(str_value):
+    str_value = str(str_value)
 
-    if (line == 'Нет стажа'):
+    if (str_value == 'Нет стажа'):
         return 0
-    elif ('менее 4 месяцев' in line):
-        return 0.3
-    elif ('4 - 6 месяцев' in line):
-        return 0.4
-    elif ('6 месяцев' in line):
-        return 0.6
-    elif ('6 месяцев - 1 год' in line):
-        return 0.9
+    elif ('менее 4 месяцев' in str_value):
+        return 3
+    elif ('4 - 6 месяцев' in str_value):
+        return 4
+    elif ('6 месяцев' in str_value):
+        return 6
+    elif ('6 месяцев - 1 год' in str_value):
+        return 9
 
-    cur_value = line.split(' ')
-    pre_num_value = list(filter(lambda x: try_to_int(x) == True, cur_value))
-    num_value = min([int(y) for y in pre_num_value])
-    return num_value
+    words_array = str_value.split(' ')
+    numeric_filtered_array = list(filter(lambda x: try_to_int(x) == True, words_array))
+    numeric_value = min([int(y) for y in numeric_filtered_array]) * 12
+    return numeric_value
 
 def try_to_int(array_el):
     try:
@@ -87,42 +96,42 @@ def try_to_int(array_el):
     except:
         return False
 
-def num_to_cat(num):
-    if (num == 0):
+def num_to_cat(numeric_value):
+    if (numeric_value == 0):
         return 'Нет стажа'
-    elif (num == 0.3):
+    elif (numeric_value == 3):
         return 'Нет стажа'
-    elif (num == 0.4):
+    elif (numeric_value == 4):
         return '4 - 6 месяцев'
-    elif (num == 0.6):
+    elif (numeric_value == 6):
         return '6 месяцев'
-    elif (num == 0.9):
+    elif (numeric_value == 9):
         return '6 месяцев - 1 год'
 
-    if (num == 1):
+    if (numeric_value >= 12 and numeric_value < 24):
         return '1 - 2 года'
-    elif (num >= 2 and num < 3):
+    elif (numeric_value >= 24 and numeric_value < 36):
         return '2 - 3 года'
-    elif (num >= 3 and num < 4):
+    elif (numeric_value >= 36 and numeric_value < 48):
         return '3 - 4 года'
-    elif (num >= 4 and num < 5):
+    elif (numeric_value >= 48 and numeric_value < 60):
         return '4 - 5 лет'
-    elif (num >= 5 and num < 6):
+    elif (numeric_value >= 60 and numeric_value < 72):
         return '5 - 6 лет'
-    elif (num >= 6 and num < 7):
+    elif (numeric_value >= 72 and numeric_value < 84):
         return '6 - 7 лет'
-    elif (num >= 7 and num < 8):
+    elif (numeric_value >= 84 and numeric_value < 96):
         return '7 - 8 лет'
-    elif (num >= 8 and num < 9):
+    elif (numeric_value >= 96 and numeric_value < 108):
         return '8 - 9 лет'
-    elif (num >= 9 and num < 10):
-        return '6 месяцев - 1 год'
-    elif (num >= 10):
+    elif (numeric_value >= 108 and numeric_value < 120):
         return '9 - 10 лет'
+    elif (numeric_value >= 120):
+        return '10 и более лет'
 
-def time_diff(d):
-  d = str(d)
-  date_only = d.split(' ')
+def time_diff(str_date):
+  str_date = str(str_date)
+  date_only = str_date.split(' ')
 
   if(len(date_only) > 1):
     del date_only[1]
@@ -130,6 +139,6 @@ def time_diff(d):
   start = datetime.fromisoformat(str(date_only[0]))
   from_now_to_start_job = datetime.now() - start
 
-  return float(from_now_to_start_job.days / 365)
+  return int(from_now_to_start_job.days / 12)
 
 create_stage("fix_errors", fix_errors_in_dataset)
