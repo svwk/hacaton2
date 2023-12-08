599e12fa0fc40f9347b5ac422738f9630c2c3705
SavSV <sv.ework@gmail.com>
2023-12-08 04:33:21 +0300

    dvc stage_fix_errors restored

--- scripts/data_scripts/fix_errors.py
+++ scripts/data_scripts/fix_errors.py
@@ -4,162 +4,191 @@
 from data_methods import create_stage
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
-import pandas as pd
 
-#Прожиточный минимум
+from data_prepare import prepare_dataset
+
+# Прожиточный минимум
 ADULT_LIVING_WAGE = 15669
 CHILD_LIVING_WAGE = 13944
 
+
 def fix_errors_in_dataset(source_dataset):
     """
-    Исправление аномалий
+    Исправление аномалий в данных
     :param source_dataset:  Исходный датасет
     """
 
     df = source_dataset.copy()
+    df = prepare_dataset(df)
 
-    df = df.apply(fix_work_age, axis=1)
+    df = df.apply(fix_seniority, axis=1)
     df = df.apply(fix_expense, axis=1)
 
     return df
 
-def fix_expense(application):
+
+def fix_expense(application_data):
     """
-    Исправление аномалий в столбце с тратами
-    :param application:  Данные заявки
+    Исправление аномальных значений расхода семьи
+    :param application_data:  Данные заявки пользователя
     """
 
+    # Реальный расход пользователя, если он один в семье
     real_expense = ADULT_LIVING_WAGE
-    child_expense = CHILD_LIVING_WAGE * application['ChildCount']
 
-    if(check_family_status(application['Family status'])):
+    # Расходы на содержание супруга/супруги при наличии
+    if check_family_status(application_data['Family status']):
         real_expense += ADULT_LIVING_WAGE
 
-    real_expense += child_expense
+    # Расходы на содержание детей
+    real_expense += CHILD_LIVING_WAGE * application_data['ChildCount']
 
-    if(real_expense > application['MonthExpense']):
-        application['MonthExpense'] = real_expense
+    # Если рассчитанные расходы превышают указанные в заявлении,
+    # исправляем на большее значение
+    if real_expense > application_data['MonthExpense']:
+        application_data['MonthExpense'] = real_expense
 
-    return application
+    return application_data
+
 
 def check_family_status(status):
     """
-        Проверка семейного статуса, если замужем или женат вернуть True
+        Проверка семейного положения, при наличии супруга/супруги
+         возвращает True
         :param status:  Данные из заявки
     """
     status = str(status)
 
-    if(status == 'Женат / замужем' or status == 'Гражданский брак / совместное проживание'):
+    if (status == 'Женат / замужем' or
+            status == 'Гражданский брак / совместное проживание'):
         return True
     else:
         return False
 
-def fix_work_age(application):
+
+def fix_seniority(application_data):
     """
-    Исправление аномалий в столбце со стажем работы
-    :param application:  Данные заявки
+    Исправление аномальных значений стажа работы
+    :param application_data:  Данные заявки
     """
 
-    #Общий стаж
-    work_age = int(values_to_num(application['Value']))
+    # Общий стаж в месяцах
+    total_seniority_in_months = int(values_to_num(application_data['Value']))
 
-    #Возраст
-    age = time_diff(application['BirthDate'])
+    # Возраст
+    age = relativedelta(datetime.today(), application_data['BirthDate'])
 
-    #Стаж работы на последнем месте
-    work_age_from_now_to_start_date = time_diff(application['JobStartDate'])
+    # Максимально возможный трудовой стаж в месяцах
+    # (возраст - 16) (ТК)
+    max_seniority_in_months = age.months + (age.years - 16) * 12
 
-    #Проверка на соотвествие трудовому зк
-    if(age < 16):
-        application['Value'] = 'Нет стажа'
-        return application
+    # Стаж работы на последнем месте
+    last_seniority = relativedelta(datetime.today(), application_data['JobStartDate'])
 
-    #Стаж на последнем рабочем месте не может быть больше чем возраст - 16 (ЗК)
-    work_age_from_now_to_start_date = min(work_age_from_now_to_start_date, age - 16)
-    #Так же как и общий стаж
-    work_age = max(work_age, age - 16)
+    # Стаж работы на последнем месте в месяцах
+    last_seniority_in_months = last_seniority.months + last_seniority.years * 12
 
-    #Общий стаж не может быть меньше, чем стаж на последнем рабочем месте
-    work_age = min(work_age, work_age_from_now_to_start_date)
+    # Проверка на соответствие трудовому законодательству
+    if age.years < 16:
+        application_data['Value'] = 'Нет стажа'
+        return application_data
 
-    application['Value'] = num_to_cat(work_age)
-    return application
+    # Стаж на последнем рабочем месте не может быть больше,
+    # чем максимально возможный трудовой стаж
+    new_last_seniority = min(last_seniority_in_months, max_seniority_in_months)
+
+    # Общий стаж не может быть больше,
+    # чем максимально возможный трудовой стаж
+    new_total_seniority = min(total_seniority_in_months, max_seniority_in_months)
+
+    # Общий стаж не может быть меньше, чем стаж на последнем рабочем месте
+    new_total_seniority = max(new_total_seniority, new_last_seniority)
+
+    if new_last_seniority != last_seniority_in_months:
+        application_data['JobStartDate'] = (date.today() -
+                                            relativedelta(months=new_last_seniority))
+
+    if new_total_seniority != total_seniority_in_months:
+        application_data['Value'] = num_to_cat(new_total_seniority)
+
+    return application_data
 
 
 def values_to_num(str_value):
     """
-    Конвертация категориального значения стажа в числовой
+    Конвертация категориального значения стажа в целое число
     :param str_value:  Данные из заявки
     """
 
     str_value = str(str_value)
 
-    if (str_value == 'Нет стажа'):
+    if str_value == 'Нет стажа':
         return 0
-    elif ('менее 4 месяцев' in str_value):
+    elif 'менее 4 месяцев' in str_value:
         return 3
-    elif ('4 - 6 месяцев' in str_value):
+    elif '4 - 6 месяцев' in str_value:
         return 4
-    elif ('6 месяцев' in str_value):
+    elif '6 месяцев' in str_value:
         return 6
-    elif ('6 месяцев - 1 год' in str_value):
+    elif '6 месяцев - 1 год' in str_value:
         return 9
 
     words_array = str_value.split(' ')
-    numeric_filtered_array = list(filter(lambda x: try_to_int(x) == True, words_array))
+    numeric_filtered_array = list(filter(lambda x: x.isdigit(), words_array))
     numeric_value = min([int(y) for y in numeric_filtered_array]) * 12
     return numeric_value
 
-def try_to_int(array_el):
-    """
-    Проверка строки на возможность конвертации в число
-    :param array_el:  Элемент массива
-    """
-    try:
-        converted = int(array_el)
-        return True
-    except:
-        return False
 
 def num_to_cat(numeric_value):
     """
     Конвертация числового значения стажа в категориальный
+    :param numeric_value:
     :param str_value:  Данные из заявки
     """
-    if (numeric_value == 0):
+    if numeric_value == 0:
         return 'Нет стажа'
-    elif (numeric_value == 3):
+    elif numeric_value == 3:
         return 'Нет стажа'
-    elif (numeric_value == 4):
+    elif numeric_value == 4:
         return '4 - 6 месяцев'
-    elif (numeric_value == 6):
+    elif numeric_value == 6:
         return '6 месяцев'
-    elif (numeric_value == 9):
+    elif numeric_value == 9:
         return '6 месяцев - 1 год'
 
-    if (numeric_value >= 12 and numeric_value < 24):
+    if 12 <= numeric_value < 24:
         return '1 - 2 года'
-    elif (numeric_value >= 24 and numeric_value < 36):
+    elif 24 <= numeric_value < 36:
         return '2 - 3 года'
-    elif (numeric_value >= 36 and numeric_value < 48):
+    elif 36 <= numeric_value < 48:
         return '3 - 4 года'
-    elif (numeric_value >= 48 and numeric_value < 60):
+    elif 48 <= numeric_value < 60:
         return '4 - 5 лет'
-    elif (numeric_value >= 60 and numeric_value < 72):
+    elif 60 <= numeric_value < 72:
         return '5 - 6 лет'
-    elif (numeric_value >= 72 and numeric_value < 84):
+    elif 72 <= numeric_value < 84:
         return '6 - 7 лет'
-    elif (numeric_value >= 84 and numeric_value < 96):
+    elif 84 <= numeric_value < 96:
         return '7 - 8 лет'
-    elif (numeric_value >= 96 and numeric_value < 108):
+    elif 96 <= numeric_value < 108:
         return '8 - 9 лет'
-    elif (numeric_value >= 108 and numeric_value < 120):
+    elif 108 <= numeric_value < 120:
         return '9 - 10 лет'
-    elif (numeric_value >= 120):
+    elif numeric_value >= 120:
         return '10 и более лет'
 
-def time_diff(date):
-    from_now_to_start_job = datetime.now() - date
+
+def time_diff(date_value):
+    """
+    Определяет разницу между некоторой датой и текущей
+    в месяцах
+    :param date_value: Дата, для которой необходимо найти разницу
+    """
+
+    from_now_to_start_job = datetime.today() - date_value
+
     return int(from_now_to_start_job.days / 30)
 
-create_stage("fix_errors", fix_errors_in_dataset)
+
+if __name__ == "__main__":
+    create_stage("fix_errors", fix_errors_in_dataset)
